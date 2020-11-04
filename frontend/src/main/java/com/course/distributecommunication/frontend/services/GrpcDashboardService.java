8fae9d8645f9fab046f2a08086b4337574340399
oharkusha <oharkusha@tradeix.com>
2020-11-05 00:07:36 +0200

    final

--- frontend/src/main/java/com/course/distributecommunication/frontend/services/GrpcDashboardService.java
+++ frontend/src/main/java/com/course/distributecommunication/frontend/services/GrpcDashboardService.java
@@ -2,11 +2,14 @@ package com.course.distributecommunication.frontend.services;
 
 import com.course.distributecommunication.frontend.dto.AuthorDto;
 import com.course.distributecommunication.frontend.dto.BookDto;
+import com.distributed.grpc.server.grpcserver.AuthorList;
 import com.distributed.grpc.server.grpcserver.AuthorServiceGrpc;
+import com.distributed.grpc.server.grpcserver.BookList;
 import com.distributed.grpc.server.grpcserver.BookServiceGrpc;
 import com.google.protobuf.Empty;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import lombok.val;
-import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,24 +18,31 @@ import java.util.stream.Collectors;
 @Service
 public class GrpcDashboardService {
 
-    @GrpcClient("authors-service")
-    private AuthorServiceGrpc.AuthorServiceBlockingStub authorServiceBlockingStub;
-
-    @GrpcClient("books-service")
-    private BookServiceGrpc.BookServiceBlockingStub bookServiceBlockingStub;
-
     public List<AuthorDto> receiveAuthors() {
-        return authorServiceBlockingStub.getAllAuthors(Empty.newBuilder().build()).getAuthorListList().stream().map(author -> {
-            val authorDto = new AuthorDto();
-            authorDto.setId(author.getId());
-            authorDto.setFirstName(author.getFirstName());
-            authorDto.setLastName(author.getLastName());
-            return authorDto;
-        }).collect(Collectors.toList());
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("authors:9091")
+                .usePlaintext()
+                .build();
+        AuthorServiceGrpc.AuthorServiceBlockingStub stub = AuthorServiceGrpc.newBlockingStub(channel);
+        AuthorList response = stub.getAllAuthors(Empty.newBuilder().build());
+        channel.shutdown();
+        return response.getAuthorListList().stream()
+                .map(author -> {
+                    val authorDto = new AuthorDto();
+                    authorDto.setId(author.getId());
+                    authorDto.setFirstName(author.getFirstName());
+                    authorDto.setLastName(author.getLastName());
+                    return authorDto;
+                })
+                .collect(Collectors.toList());
     }
 
     public List<BookDto> receiveBooks() {
-        return bookServiceBlockingStub.getAllBooks(Empty.newBuilder().build()).getBookListList().stream().map(book -> {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("books:9092")
+                .usePlaintext()
+                .build();
+        BookServiceGrpc.BookServiceBlockingStub stub = BookServiceGrpc.newBlockingStub(channel);
+        BookList response = stub.getAllBooks(Empty.newBuilder().build());
+        return response.getBookListList().stream().map(book -> {
             val bookDto = new BookDto();
             bookDto.setId(book.getId());
             bookDto.setTitle(book.getTitle());
